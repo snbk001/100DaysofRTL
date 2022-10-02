3f0a0f2dc87d955f67c3079c8c002aeaee6161fd
snbk001 <54323691+snbk001@users.noreply.github.com>
2022-10-02 19:37:59 +0530

    Test bench for logic gates

---
+++ day1/logicgates_tb.sv
@@ -0,0 +1,42 @@
+module logicgates_tb();
+
+reg i_a;
+reg i_b;
+wire o_and;
+wire o_or;
+wire o_xor;
+
+logicgates dut (
+	.i_a(i_a),
+	.i_b(i_b),
+	.o_and(o_and),
+	.o_or(o_or),
+	.o_xor(o_xor)
+	);
+	
+initial begin
+	// Dump waves
+    	$dumpfile("logicgates.vcd");
+    	$dumpvars(0, logicgates_tb);	
+end
+
+initial begin
+	repeat(20)
+		begin
+			stimulus();
+			$display("i_a : %0b, i_b : %0b, o_and : %0b, o_or : %0b, o_xor : %0b", i_a, i_b, o_and, o_or, o_xor);
+		end
+end
+
+task stimulus();
+	begin
+		i_a = $random;#5;
+		i_b = $random;#5;
+	end
+endtask
+
+initial begin
+	#200 $finish;
+end
+
+endmodule
