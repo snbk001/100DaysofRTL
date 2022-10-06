80d3139af3b79689f0b10a2a0c36ae9f0f0c54dc
snbk001 <54323691+snbk001@users.noreply.github.com>
2022-10-06 21:48:18 +0530

    8 bit counter

---
+++ day6/counter8bit.sv
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+module counter8bit(
+	clk,
+	reset,
+	o_count
+	);
+	
+//port declaration
+input logic clk;
+input logic reset;
+output logic [7:0] o_count;
+
+always_ff @ (posedge clk)
+	begin
+		if (reset)
+			o_count <= 8'b0;
+		else 
+			begin
+				if (o_count < 8'b11111111)
+					o_count <= o_count + 1'b1;
+				else
+					o_count <= 8'b0;
+			end
+	end
+endmodule
