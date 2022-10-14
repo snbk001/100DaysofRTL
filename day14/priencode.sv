7f899d2bd4353ee987a6d5d5dc69450a4b2e714f
Sudhee Bhat <54323691+snbk001@users.noreply.github.com>
2022-10-14 17:39:40 +0530

    Update priencode.sv

--- day14/priencode.sv
+++ day14/priencode.sv
@@ -9,13 +9,13 @@ output logic [2:0] o_y;
 
 always_comb
 	begin
-			casez(i_d)
-				4'b0000: o_y = 3'b000;
-				4'b0001: o_y = 3'b001;
-				4'b001?: o_y = 3'b010;
-				4'b01??: o_y = 3'b011;
-				4'b1???: o_y = 3'b100;
-			endcase
+		casez(i_d)
+			4'b0000: o_y = 3'b000;
+			4'b0001: o_y = 3'b001;
+			4'b001?: o_y = 3'b010;
+			4'b01??: o_y = 3'b011;
+			4'b1???: o_y = 3'b100;
+		endcase
 	end
 
 endmodule
